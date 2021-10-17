5aefbd11e8438d28594d54272dc53e6d3149e9f2
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2021-10-16 21:28:59 -0300

    bueno hoy aprendi cosas de scope del stylo y los componentes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ComponenteDePruebas></ComponenteDePruebas>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ComponenteDePruebas from "./components/ComponenteDePruebas";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ComponenteDePruebas
   }
 }
 </script>
---
+++ src/components/ComponenteDePruebas.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 id="jajasaludos">Jaja Saludos XDXDXDXd</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComponenteDePruebas"
+}
+</script>
+
+<style scoped>
+  #jajasaludos {
+    color: red;
+  }
+</style>
