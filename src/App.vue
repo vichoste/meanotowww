bd3c2527f08b144102e0ec03a27ffb5176480f49
Felipe <felipe.olave.r@hotmail.com>
2021-10-19 22:43:57 -0300

    Interfaz de login
    Interfaz de login lista.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- CSS only -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <title>Inicio</title>
+</head>
+<body>
+  <Login/>
+</body>
+</html>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Login from './components/Login.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Login,
   }
+    
 }
 </script>
 
