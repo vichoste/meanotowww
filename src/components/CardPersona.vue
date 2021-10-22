e5e0b98f6037221602fbebdc75dccea800fdcda7
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2021-10-22 19:24:57 -0300

    v-for y v-bind

---
+++ src/components/CardPersona.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{nombre}}</h1>
+    <h2>{{edad}}</h2>
+    <p>
+      -------------*-------------
+      -------------*-------------
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardPersona",
+  props:{
+    nombre:String,
+    edad:Number
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/ComponenteDePruebas.vue
+++ src/components/ComponenteDePruebas.vue
@@ -2,11 +2,26 @@
   <div>
     <h1 id="jajasaludos">Jaja Saludos XDXDXDXd</h1>
   </div>
+
+  <ul v-for="(c, index) in personas" :key="c.nombre">
+        <li> {{index}}. Nombre: {{c.nombre}}, Edad: {{c.edad}}</li>
+  </ul>
+
+  <CardPersona v-for="c in personas" :key="c.nombre" v-bind:nombre="c.nombre" v-bind:edad="c.edad"></CardPersona>
+
 </template>
 
 <script>
+import CardPersona from "./CardPersona";
 export default {
-  name: "ComponenteDePruebas"
+  name: "ComponenteDePruebas",
+  components: {CardPersona},
+  data()
+  {
+    return {
+      personas : [{nombre: "juan", edad: 10}, {nombre: "pepe", edad: 15}]
+    }
+  }
 }
 </script>
 
