2adb53f162691f116d77b98e1e97da862474bbac
Felipe <felipe.olave.r@hotmail.com>
2021-11-04 19:37:54 -0300

    cambios con ruta
    oezi

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,10 @@
 <template>
-  <!-- CSS only -->
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <title>Inicio</title>
-</head>
-<body>
-  <Login/>
-</body>
-</html>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import Login from './components/Login.vue'
-export default {
-  name: 'App',
-  components:{
-    Login,
-  }
-    
-}
-</script>
 
 <style>
 #app {
@@ -34,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
