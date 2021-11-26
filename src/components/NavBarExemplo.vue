7c5d8714733d4c5aff7a0e1d8cace96921eafb7e
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-11-26 11:34:01 -0300

    Navbar stuff

--- src/components/NavBarExemplo.vue
+++ src/components/NavBarExemplo.vue
@@ -1,20 +1,24 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
       <router-link class="navbar-brand" to="/">Me Anoto</router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav ms-auto">
           <li class="nav-item">
             <router-link class="nav-link active" to="/about">About</router-link>
           </li>
         </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>
@@ -22,10 +26,9 @@
 
 <script>
 export default {
-  name: "NavBarExemplo"
-}
+  name: "NavBarExemplo",
+};
 </script>
 
 <style scoped>
-
 </style>
